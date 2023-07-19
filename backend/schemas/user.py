f4207990d5b5580cde77a75eb044d2711d37773b
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-19 17:19:48 +0330

    Add seperate ``schema`` for displaying user

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -2,4 +2,13 @@ from pydantic import BaseModel, EmailStr, Field
 
 class User(BaseModel):
     email: EmailStr
-    password: Field(..., min_length=4)
+    # password: Field(..., min_length=4)
+    password: str
+
+class UserShow(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config:
+        orm_mode = True
