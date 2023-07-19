55535eaa9fabf925458d722cab5112d8f5f179c1
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-19 13:04:56 +0330

    Add ``get_db`` generator

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,3 +1,4 @@
+from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,3 +9,10 @@ SQLALCHEMY_DB_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        return db
+    finally:
+        db.close()
