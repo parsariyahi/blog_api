175db4805b6d3a2f5697e2d040bd566f8b333805
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-19 17:20:18 +0330

    Add ``create_user`` function and an orm repository pattern

---
+++ backend/db/repository/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+
+from schemas.user import User as UserSchema
+from db.base import User as UserModel
+from core.hashing import Hasher
+
+
+def create_user(user: UserSchema, db: Session) -> UserModel:
+    user = UserModel(
+        email = user.email,
+        password = Hasher.hash(user.password),
+        is_active=True,
+        is_superuser=False
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
