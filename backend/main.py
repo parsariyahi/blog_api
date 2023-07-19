66356c9ed5e6cda2a8d58d4b618307d447fb4a79
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-19 17:20:57 +0330

    Refactored ``main.py`` and make it runable with ``__name__``

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from uvicorn import run
+
+from apis.base import api_router
 from core.config import settings
 from db.session import engine
 from db.base import Base
@@ -8,10 +11,14 @@ from db.base import Base
 def create_tables():         
 	Base.metadata.create_all(bind=engine)
         
+def include_router(app):
+    app.include_router(api_router)
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME,version=settings.VERSION)
     create_tables()
+    include_router(app)
     return app
 
 
@@ -21,3 +28,7 @@ app = start_application()
 @app.get("/")
 def home():
     return {"msg":"Hello FastAPI🚀"}
+
+
+if __name__ == "__main__":
+    run(app, host="127.0.0.1", port=8000)
