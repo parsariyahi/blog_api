91a761f14448b594eb9f71ca948616542a743674
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-20 12:49:11 +0330

    Migrations for ``blog``

---
+++ backend/alembic/versions/c05d2a225196_add_unique_to_slug.py
@@ -0,0 +1,24 @@
+"""Add unique to slug
+
+Revision ID: c05d2a225196
+Revises: 0a7e40525d6f
+Create Date: 2023-07-20 12:07:07.123357
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c05d2a225196'
+down_revision = '0a7e40525d6f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
