ec577d44688d45069e0f5c67bfbca3c462bf9ab8
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-22 13:39:48 +0330

    Add ``edit_blog`` in ``blog`` routes

--- backend/apis/v1/blog_routes.py
+++ backend/apis/v1/blog_routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 from db.session import get_db
-from db.repository.blog import create_blog, retrive_blog, list_blogs
+from db.repository.blog import create_blog, retrive_blog, update_blog, list_blogs
 from schemas.blog import BlogShow, Blog as BlogSchema
 
 router = APIRouter()
@@ -17,6 +17,15 @@ def publish_blog(blog: BlogSchema, db: Session = Depends(get_db)):
 @router.get("/{blog_id}/get", response_model=BlogShow, status_code=status.HTTP_200_OK)
 def get_blog(blog_id: int, db: Session = Depends(get_db)):
     blog = retrive_blog(blog_id, db)
+
+    if not blog:
+        raise HTTPException(detail=f"Blog with id: {blog_id} does not exists", status_code=status.HTTP_404_NOT_FOUND)
+
+    return blog
+
+@router.put("/{blog_id}/edit", response_model=BlogShow, status_code=status.HTTP_200_OK)
+def edit_blog(blog_id: int, blog: BlogSchema, db: Session = Depends(get_db)):
+    blog = update_blog(blog_id, blog, 1, db)
 
     if not blog:
         raise HTTPException(detail=f"Blog with id: {blog_id} does not exists", status_code=status.HTTP_404_NOT_FOUND)
