df9473c7684c02427557546013045910c1f5d978
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-22 11:43:32 +0330

    Refactored default value of ``is_active`` field for ``blog`` table

---
+++ backend/alembic/versions/97e69c27f3d2_change_defualt_value_for_blog_is_active.py
@@ -0,0 +1,24 @@
+"""Change defualt value for blog.is_active
+
+Revision ID: 97e69c27f3d2
+Revises: 94a4ef07b46c
+Create Date: 2023-07-22 11:12:59.254763
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '97e69c27f3d2'
+down_revision = '94a4ef07b46c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- backend/db/models/blog.py
+++ backend/db/models/blog.py
@@ -13,4 +13,4 @@ class Blog(Base):
     author_id =  Column(Integer, ForeignKey("user.id"))
     author = relationship("User", back_populates="blogs")
     created_at = Column(DateTime, default=datetime.now)
-    is_active = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
