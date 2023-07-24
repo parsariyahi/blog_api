f58e9b2c4d9de1f7d0c0689d59331ee5da1b9709
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-24 17:30:03 +0330

    Add ``router`` from ``token_routes`` into ``base`` route

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 
 # from apis.v1.user_routes import router as user_router
 # from apis.v1.blog_routes import  router as blog_router
-from apis.v1 import blog_routes, user_routes
+from apis.v1 import blog_routes, user_routes, token_routes
 
 
 api_router = APIRouter()
 
 api_router.include_router(user_routes.router,prefix="/users",tags=["users"])
 api_router.include_router(blog_routes.router,prefix="/blogs",tags=["blogs"])
+api_router.include_router(token_routes.router,prefix="/auth",tags=["auth"])
