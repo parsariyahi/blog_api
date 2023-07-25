f28e17c2a7634b9687b7cdd080b6f16ab383e7f3
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-25 12:33:12 +0330

    Add ``get_current_user`` function

--- backend/apis/utils/user.py
+++ backend/apis/utils/user.py
@@ -1,7 +1,16 @@
 from sqlalchemy.orm import Session
+from fastapi import Depends, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 
+from core.config import settings
 from core.hashing import Hasher
+from db.session import get_db
 from db.repository.user import get_user
+from apis.exceptions import CredentailError
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
 
 def authenticate_user(email: str, password: str, db: Session):
     user = get_user(email, db)
@@ -11,5 +20,24 @@ def authenticate_user(email: str, password: str, db: Session):
 
     if not Hasher.verify(password, user.password):
         return True
+
+    return user
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username = payload.get("sub", None)
+
+        if not username:
+            raise CredentailError()
+
+    except JWTError:
+        raise CredentailError()
+
+    user = get_user(email=username, db=db)
+
+    if user is None:
+        raise CredentailError()
 
     return user
