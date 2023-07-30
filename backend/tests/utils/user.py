5bca660a4447e99adaecc1af4994d4b819685bae
parsariyahi <pany.parsariyahi@gmail.com>
2023-07-30 11:09:47 +0330

    Add parameters for ``email``, ``password`` in ``create_random_user``

--- backend/tests/utils/user.py
+++ backend/tests/utils/user.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from db.repository.user import  create_user
 from schemas.user import User as UserSchema
 
-def create_random_user(db: Session):
+def create_random_user(db: Session, email="test@example.com", password="test1234"):
     user = UserSchema(
-        email="test@example.com",
-        password="test1234",
+        email=email,
+        password=password,
     )
     user = create_user(user, db)
 
