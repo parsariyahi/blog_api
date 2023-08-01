6d6f35587d9bf72157087a823594f5f4eee91fcc
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-01 15:10:25 +0330

    Add ``force_authentication`` for tests

--- backend/tests/utils/user.py
+++ backend/tests/utils/user.py
@@ -11,3 +11,12 @@ def create_random_user(db: Session, email="test@example.com", password="test1234
     user = create_user(user, db)
 
     return user
+
+
+def force_authentication(client, user) -> str:
+    response = client.post("/auth/token", data=user)
+
+    token = response.json()["access_token"]
+
+    return token
+
