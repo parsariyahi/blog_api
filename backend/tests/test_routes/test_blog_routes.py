9f1e8b9f0424c35bbe20911ee0fe6d9b23addffb
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-01 15:11:06 +0330

    Add tests for ``create blog route``

--- backend/tests/test_routes/test_blog_routes.py
+++ backend/tests/test_routes/test_blog_routes.py
@@ -1,4 +1,28 @@
 from tests.utils.blog import create_random_blog
+from tests.utils.user import create_random_user
+from tests.utils.user import force_authentication
+
+
+def test_create_a_blog(client, db_session, access_token):
+    
+    headers= {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    blog = {
+        "title": "test blog",
+        "content": "this is a test blog",
+    }
+
+    response = client.post("/blogs/add", json=blog, headers=headers)
+    json_response = response.json()
+
+    assert response.status_code == 201
+    assert json_response["title"] == blog["title"]
+    assert json_response["content"] == blog["content"]
+    assert json_response["slug"] == "test-blog"
 
 def test_fetch_a_blog(client, db_session):
     blog = create_random_blog(db_session)
