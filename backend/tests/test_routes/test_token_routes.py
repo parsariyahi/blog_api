f6513bd5a336148f09a52a766de42360ff520665
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-05 10:30:06 +0330

    Refactore ``email``, ``password`` in all tests, read it from ``core.config.settings``

--- backend/tests/test_routes/test_token_routes.py
+++ backend/tests/test_routes/test_token_routes.py
@@ -1,20 +1,19 @@
+from core.config import settings
 from tests.utils.user import create_random_user
 from tests.utils.token import verify_jwt_token_sub
 
 def test_generate_token(client, db_session):
-    email = "test@some.com"
-    password = "test123456"
-    user = create_random_user(db_session, email=email, password=password)
+    user = create_random_user(db_session, email=settings.TEST_USER_EMAIL, password=settings.TEST_USER_PASSWORD)
 
     payload = {
-        "username": email,
-        "password": password,
+        "username": settings.TEST_USER_EMAIL,
+        "password": settings.TEST_USER_PASSWORD,
     }
 
     response = client.post("/auth/token", data=payload)
     json_response = response.json()
 
-    access_token_sub = email
+    access_token_sub = settings.TEST_USER_EMAIL
 
     access_token = json_response["access_token"]
 
--- backend/tests/test_routes/test_user_routes.py
+++ backend/tests/test_routes/test_user_routes.py
@@ -1,6 +1,12 @@
+from core.config import settings
+
 def test_create_user(client):
-    data = {"email":"testuser@nofoobar.com","password":"testing"}
+    data = {
+        "email": settings.TEST_USER_EMAIL,
+        "password": settings.TEST_USER_PASSWORD,
+    }
     response = client.post("/users/add",json=data)
+
     assert response.status_code == 201
     assert response.json()["email"] == "testuser@nofoobar.com"
     assert response.json()["is_active"] == True
