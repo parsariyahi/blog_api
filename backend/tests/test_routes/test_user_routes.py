8cd26509dc58ff7429718e43cf9b4f8666d4a39c
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-05 10:34:08 +0330

    Refactore ``email``, ``password`` in all tests, read it from ``core.config.settings``

--- backend/tests/test_routes/test_user_routes.py
+++ backend/tests/test_routes/test_user_routes.py
@@ -8,5 +8,5 @@ def test_create_user(client):
     response = client.post("/users/add",json=data)
 
     assert response.status_code == 201
-    assert response.json()["email"] == "testuser@nofoobar.com"
+    assert response.json()["email"] == settings.TEST_USER_EMAIL
     assert response.json()["is_active"] == True
